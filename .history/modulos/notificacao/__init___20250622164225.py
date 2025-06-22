eb037b005eedb836c87184dc40e1c2f8529ca5a5
Diogo Adario Marassi <107212374+DiogoMarassi@users.noreply.github.com>
2025-06-22 17:40:35 -0300

    Delete .history directory

--- .history/modulos/notificacao/__init___20250622164225.py
+++
@@ -1,4 +0,0 @@
-# Define as funções públicas do módulo
-__all__ = ["listarNotificacoes", "salvarNotificacao", "enviarNotificacao"]
-
-from .notificacao import listarNotificacoes, salvarNotificacao, enviarNotificacao
